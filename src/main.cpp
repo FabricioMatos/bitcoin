46c6f5cef2cb0dbc40cf1ebfc0102c0a3fce9493
Fabricio Matos <fabriciomatos@Fabricios-MacBook-Pro.local>
2014-12-08 01:54:17 -0200

    XCode project created

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2014-2015 Fabricio Matos & Loyalty Coins Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 
 #include "main.h"
 
